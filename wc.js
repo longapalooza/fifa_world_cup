8409792ba8a3c0621261ffe53fec8486bbaafcca
Will Long <long@latech.edu>
2026-06-27 12:37:07 -0500

    Update wc.js

--- wc.js
+++ wc.js
@@ -215,7 +215,7 @@ document.addEventListener('DOMContentLoaded', () => {
             content += '<img src="';
             content += pre_img_url;
             content += pick[i]['abbr'];
-            content += post_img_url;
+            //content += post_img_url;
             content += '" />&nbsp;&nbsp;&nbsp;&nbsp;';
             content += '(' + pick[i]['seed'] + ')&nbsp;';
             content += pick[i]['name'];
