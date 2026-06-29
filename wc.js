8ab25eddf444c05f5d9b09d5cbd3bc62ae7855cf
Will Long <long@latech.edu>
2026-06-28 20:39:33 -0500

    Update wc.js

--- wc.js
+++ wc.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     document.getElementById('close_time').innerHTML = deadline
     
-    if(true || current_time.getTime() < close_time.getTime()){
+    if(current_time.getTime() < close_time.getTime()){
         var game_index = 0;
         var n_games = teams.length - 1;
 
